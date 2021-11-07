869a4fc909f2a231cc86af4a88575bc1a7402c46
gaoyunfeng <gaoyunfeng1995@gmail.com>
2021-11-07 21:52:14 +0800

    kafka-client测试

---
+++ kafka/src/main/java/com/githug/yunfeng/kafka/Main.java
@@ -0,0 +1,64 @@
+package com.githug.yunfeng.kafka;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class Main {
+    private static final String TOPIC = "test";
+
+    public static void main(String[] args) {
+        producer();
+        customer();
+    }
+
+    public static void producer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 10);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<>(TOPIC, Integer.toString(i), Integer.toString(i)));
+        }
+        producer.close();
+    }
+
+    public static void customer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "false");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(TOPIC));
+        final int minBatchSize = 2;
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records) {
+                buffer.add(record);
+                System.out.println(record.value());
+            }
+            if (buffer.size() >= minBatchSize) {
+                consumer.commitSync();
+                buffer.clear();
+            }
+        }
+    }
+}
