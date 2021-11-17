5c87cfe0d97dafb846c89d8943f664d8184d9783
gaoyunfeng <gaoyunfeng1995@gmail.com>
2021-11-17 23:29:22 +0800

    okhttp发送表单请求，增加headers

--- restful/src/main/java/com/github/yunfeng/okhttp/FormRequest.java
+++ restful/src/main/java/com/github/yunfeng/okhttp/FormRequest.java
@@ -1,9 +1,12 @@
 package com.github.yunfeng.okhttp;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import okhttp3.Call;
 import okhttp3.FormBody;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -19,7 +22,11 @@ public class FormRequest {
             .add("key2", "value2")
             .build();
 
-        Request request = new Request.Builder().url(url).post(body).build();
+        Map<String, String> headerMap = new HashMap<>();
+        headerMap.put("key3", "value3");
+        Headers headers = Headers.of(headerMap);
+
+        Request request = new Request.Builder().url(url).headers(headers).post(body).build();
 
         Call call = okHttpClient.newCall(request);
         try {
