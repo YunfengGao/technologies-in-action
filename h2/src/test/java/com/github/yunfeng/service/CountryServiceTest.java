17a96e89872ca850d61088cabf7f59fb3c649bc1
gaoyunfeng <gaoyunfeng1995@gmail.com>
2022-08-16 23:02:34 +0800

    使用DirtiesContext解决插入的脏数据问题

--- h2/src/test/java/com/github/yunfeng/service/CountryServiceTest.java
+++ h2/src/test/java/com/github/yunfeng/service/CountryServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,11 +19,19 @@ class CountryServiceTest {
     private CountryService countryService;
 
     @Test
-    @DisplayName("测试保存城市")
-    @Transactional
+    @DisplayName("测试保存城市后，清理context，防止插入的数据影响其它UT")
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
     void saveCountry() {
         Country country = countryService.saveCountry(Country.builder().id(6L).name("CHINA").build());
         Assertions.assertEquals(6, country.getId());
+    }
+
+    @Test
+    @DisplayName("测试保存城市后，使用事务回滚，防止插入的数据影响其它UT")
+    @Transactional
+    void saveCountry2() {
+        Country country = countryService.saveCountry(Country.builder().id(7L).name("RUSSIA").build());
+        Assertions.assertEquals(7, country.getId());
     }
 
     @Test
